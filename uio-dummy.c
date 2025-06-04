94b1eb5350e387475daa385e8504a75dff6d4831
pip-izony <eeodqql09@gmail.com>
2025-06-04 04:37:57 -0400

    Implement PKU & Add userspace program

--- uio-dummy.c
+++ uio-dummy.c
@@ -83,17 +83,145 @@
 #include <linux/uio_driver.h>
 #include <linux/version.h>
 #include <linux/vmalloc.h>
+#include <linux/mm.h>
+#include <asm/page.h>
+#include <linux/pid.h>
+
+#include <asm/siginfo.h>
+#include <linux/rcupdate.h>
+#include <linux/sched.h>
+
+#define ORDER get_order(mem_size)
+#define MAXIMUM_NUM_PKEY 16
+#define PROT_READ        0x1                /* Page can be read.  */
+#define PROT_WRITE       0x2                /* Page can be written.  */
+#define PROT_EXEC        0x4                /* Page can be executed.  */
+#define PROT_NONE        0x0                /* Page can not be accessed.  */
+
+struct pkey_entry {
+	pid_t pid;
+	unsigned long permission;
+	struct list_head list;
+};
+static LIST_HEAD(pkey_list);
+static unsigned long current_permission = -1;
 
 static struct uio_info *info;
 static struct device *dev;
-static unsigned long long mem_size = 2692759552 + 32;
+static unsigned long long mem_size = 16 * 1024;
 static bool irqs_enabled = false;
 
 module_param(mem_size, ullong, S_IRUGO);
 
+static inline phys_addr_t page_to_phys(const struct page *page) {
+    return (phys_addr_t)page_to_pfn(page) << PAGE_SHIFT;
+}
+
+static struct pkey_entry *find_pkey(pid_t pid) {
+	struct pkey_entry *entry;
+
+    list_for_each_entry(entry, &pkey_list, list) {
+        if (entry->pid == pid)
+            return entry;
+    }
+    return NULL;
+}
+
+void print_pkey(void) {
+	struct pkey_entry *entry;
+
+    list_for_each_entry(entry, &pkey_list, list) {
+		printk(KERN_INFO "[uio-dummy] pid : %d\n", entry->pid);
+	}
+}
+
+void free_pkey(pid_t pid) {
+    struct pkey_entry *entry, *tmp;
+    struct task_struct *task;
+    struct kernel_siginfo info;
+
+    list_for_each_entry_safe(entry, tmp, &pkey_list, list) {
+        if (entry->pid == pid) {
+			memset(&info, 0, sizeof(info));
+			info.si_signo = SIGSEGV;
+			info.si_int = -1;
+            task = pid_task(find_vpid(pid), PIDTYPE_PID);
+            if (task)
+                send_sig_info(SIGSEGV, &info, task);
+            list_del(&entry->list);
+			kfree(entry);
+        }
+    }
+}
+
+void free_all_pkeys(void) {
+    struct pkey_entry *entry, *tmp;
+	struct task_struct *task;
+
+	print_pkey();
+
+    list_for_each_entry_safe(entry, tmp, &pkey_list, list) {
+		struct kernel_siginfo info;
+
+		memset(&info, 0, sizeof(info));
+		info.si_signo = SIGSEGV;
+		info.si_int = -1;
+		task = pid_task(find_vpid(entry->pid), PIDTYPE_PID);
+		if (task)
+			send_sig_info(SIGSEGV, &info, task);
+        list_del(&entry->list);
+		kfree(entry);
+    }
+}
+
+static int uio_dummy_mmap(struct uio_info *info, struct vm_area_struct *vma) {
+	pid_t current_pid = task_pid_nr(current);
+	struct pkey_entry *entry;
+	struct uio_mem *mem = &info->mem[0];
+    unsigned long pfn = mem->addr >> PAGE_SHIFT;
+    unsigned long size = vma->vm_end - vma->vm_start;
+	unsigned long permission = 0;
+
+    if (size > mem->size)
+        return -EINVAL;
+
+	if (vma->vm_flags & VM_READ) {
+    	permission = PROT_READ;
+	}
+
+	if (vma->vm_flags & VM_WRITE) {
+    	permission |= PROT_WRITE;
+	}
+	
+	entry = find_pkey(current_pid);
+	if (entry) {
+		printk(KERN_INFO "[uio-dummy] TODO: Updated permission %d for pid %d\n", permission, current_pid);
+	}
+	else {
+		entry = kmalloc(sizeof(*entry), GFP_KERNEL);
+		entry->pid = current_pid;
+		entry->permission = permission;
+		if (current_permission != permission) {
+			current_permission = permission;
+			free_all_pkeys();
+		}
+		else if (current_permission < 0) {
+			current_permission = permission;
+		}
+		list_add(&entry->list, &pkey_list);
+
+		print_pkey();
+		printk(KERN_INFO "[uio-dummy] Added permission %d for pid %d\n", permission, current_pid);
+	}
+
+	printk(KERN_INFO "[uio-dummy] the memory is mmaped\n");
+	
+	return remap_pfn_range(vma, vma->vm_start, pfn, size, vma->vm_page_prot);
+}
+
 static void my_release(struct device *dev)
 {
-	printk(KERN_INFO "releasing my uio device\n");
+	printk(KERN_INFO "[uio-dummy] releasing my uio device\n");
 }
 
 static int uio_dummy_proc_show(struct seq_file *m, void *v)
@@ -112,14 +240,45 @@ static int uio_dummy_proc_open(struct inode *inode, struct file *file)
 static ssize_t uio_dummy_proc_write(struct file *file, const char __user *ubuf,
 				    size_t count, loff_t *ppos)
 {
-	if (irqs_enabled) {
-		printk(KERN_DEBUG
-		       "Triggering event through write of proc file\n");
-		uio_event_notify(info);
-	} else {
-		printk(KERN_DEBUG "User did not enable IRQs, not firing\n");
+	pid_t current_pid = task_pid_nr(current);
+	struct pkey_entry *entry;
+	char buf[16];
+	char *endptr;
+	unsigned long permission;
+
+	if (copy_from_user(buf, ubuf, count))
+		return -EFAULT;
+	
+	for (int i = 0; i < sizeof(buf); i++) {
+		if (buf[i] == '1') {
+			permission = 1;
+			break;
+		}
+		else if (buf[i] == '2') {
+			permission = 2;
+			break;
+		}
+		else if (buf[i] == '3') {
+			permission = 3;
+			break;
+		}
 	}
 
+	printk(KERN_INFO "[uio-dummy] Userspace requested permission %d\n", permission);
+
+	entry = find_pkey(current_pid);
+	if (!entry) {
+		free_pkey(current_pid);
+		printk(KERN_INFO "[uio-dummy] Permission is not allocated %d for pid %d\n", permission, current_pid);
+		return -EINVAL;
+	}
+	
+	if (entry->permission != permission || current_permission != permission) {
+		free_pkey(current_pid);
+		printk(KERN_INFO "[uio-dummy] Wrong permission %d for pid %d\n", permission, current_pid);
+		return -EINVAL;
+	}
+	
 	return count;
 }
 
@@ -144,8 +303,15 @@ static struct proc_ops uio_dummy_proc_operations = {
 
 static int uio_dummy_irq_control(struct uio_info *dev_info, s32 irq_on)
 {
-	printk(KERN_INFO "Userspace requested IRQ generation %d\n", irq_on);
+	pid_t current_pid = task_pid_nr(current);
+
+	printk(KERN_INFO "[uio-dummy] Userspace requested IRQ generation %d\n", irq_on);
 	irqs_enabled = irq_on != 0;
+
+	if (!(current_permission & PROT_WRITE)) {
+		printk(KERN_INFO "[uio-dummy] There is no write permission\n");
+		free_pkey(current_pid);
+	}
 
 	return 0;
 }
@@ -170,6 +336,14 @@ static int __init uio_dummy_init(void)
 	// file written to
 	info->irq = UIO_IRQ_CUSTOM;
 	info->irqcontrol = uio_dummy_irq_control;
+	info->mmap = uio_dummy_mmap;
+
+	// Make sure the physical memory is continuous (for PKU)
+	struct page *pages = alloc_pages(GFP_KERNEL, ORDER);
+	if (!pages) {
+		printk(KERN_INFO "[uio-dummy] alloc_pages error!\n");
+		return -1;
+	}
 
 	// Just allocate a slab of virtual memory to be exposed through mmap
 	mem = &info->mem[0];
@@ -177,16 +351,17 @@ static int __init uio_dummy_init(void)
 	mem->addr = (phys_addr_t)vmalloc(mem_size);
 	mem->size = mem_size;
 	mem->name = "UIO dummy memory block";
+	printk(KERN_INFO "[uio-dummy] virtual memory address 0x%llx\n", (unsigned long long)mem->addr);
 
 	if (uio_register_device(dev, info) < 0) {
 		vfree((const void *)mem->addr);
 		device_unregister(dev);
 		kfree(dev);
 		kfree(info);
-		printk(KERN_INFO "Failing to register uio device\n");
+		printk(KERN_INFO "[uio-dummy] Failing to register uio device\n");
 		return -1;
 	}
-	printk(KERN_INFO "Allocating %llu bytes for mem0", mem_size);
+	printk(KERN_INFO "[uio-dummy] Allocating %llu bytes for mem0\n", mem_size);
 
 	proc_create_data("uio-dummy", 0666, NULL, &uio_dummy_proc_operations,
 			 dev);
@@ -200,7 +375,7 @@ static void __exit uio_dummy_exit(void)
 	vfree((const void *)mem->addr);
 	device_unregister(dev);
 	remove_proc_entry("uio-dummy", NULL);
-	printk(KERN_INFO "Un-Registered UIO handler\n");
+	printk(KERN_INFO "[uio-dummy] Un-Registered UIO handler\n");
 	kfree(info);
 	kfree(dev);
 }
---
+++ user_program/main.c
@@ -0,0 +1,98 @@
+#define _GNU_SOURCE
+#include <sys/ioctl.h>
+#include <sys/mman.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAXIMUM_NUM_PKEY 16
+
+int dummy_pkey_mprotect(void *addr, size_t len, int permission, int pkey) {
+    int ret;
+    char buf[16];
+    int fd = open("/proc/uio-dummy", O_RDWR);
+    snprintf(buf, sizeof(buf), "%d", permission);
+    ssize_t wlen = write(fd, buf, strlen(buf));
+
+    ret = pkey_mprotect(addr, len, permission, pkey);
+    if (ret == -1) {
+        printf("[uio user] pkey_mprotect failed!\n");
+        exit(1);
+    }
+    return ret;
+}
+
+int main(int argc, char *argv[]) {
+    int count = 0;
+    int fd;
+    int pkey, status;
+    int permission;
+    void *buf;
+    size_t length = 16 * 1024;
+    char spinner[] = {'|', '/', '-', '\\'};
+
+    if (argc < 2) {
+        printf("Usage: sudo ./user_program <permission>\n");
+        return EXIT_FAILURE;
+    }
+    
+    permission = atoi(argv[1]);
+
+    /* mapping with kernel memory */
+    fd = open("/dev/uio0", O_RDWR);
+    if (fd < 0) {
+        printf("[uio user] open error\n");
+        return EXIT_FAILURE;
+    }
+
+    /* Allocate one page of memory */
+    buf = mmap(NULL, length, permission, MAP_SHARED, fd, 0);
+    if (buf == MAP_FAILED) {
+        printf("[uio user] mmap failed!\n");
+        return EXIT_FAILURE;
+    }
+
+    printf("[uio user] /dev/uio0 memory mapped at %p\n", buf);
+
+    /* Allocate a protection key */
+    pkey = pkey_alloc(0, 0);
+    if (pkey == -1) {
+	printf("[uio user] pkey_alloc failed!\n");
+	return EXIT_FAILURE;
+    }
+
+    status = dummy_pkey_mprotect(buf, length, permission, pkey);
+
+    status = pkey_set(pkey, 0);
+    if (status == -1) {
+	printf("[uio user] pkey_set failed!\n");
+	return EXIT_FAILURE;
+    }
+
+    if (munmap(buf, length) < 0) {
+	printf("munmap failed!\n");
+        return EXIT_FAILURE;
+    }
+
+    /* write example */
+    int irq_on = 1;
+    ssize_t wlen = write(fd, &irq_on, sizeof(irq_on));
+    printf("Write Success!\n");
+
+    printf("Waiting...");
+    while (1) {
+	count++;
+        printf("\b%c", spinner[count]);
+        fflush(stdout);
+        usleep(200000);
+	count = (count + 1) % 4;
+    }
+
+    close(fd);
+
+    return EXIT_SUCCESS;
+}
